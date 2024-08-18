6bae8fd5b8f93801410a0f85d4f5be4fff00c2cb
Aleksey Zubovich <C:\Users\alexz\AppData\Roaming\The Bat!>
2024-08-18 21:02:41 +0300

    Add CodeWars task - 53ee5429ba190077850011d4

---
+++ sandbox/codewars/53ee5429ba190077850011d4.js
@@ -0,0 +1,7 @@
+/* You Can't Code Under Pressure #1 - 53ee5429ba190077850011d4 */
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    i *= 2;
+    return i;
+}
